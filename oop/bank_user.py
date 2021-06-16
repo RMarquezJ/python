e0d8aa3fab1a3df1541a95f3985135bc18365edb
Rodrigo Marquez <rod.m89@gmail.com>
2021-06-15 20:54:22 -0400

    Added multiple account option per user

--- oop/bank_user.py
+++ oop/bank_user.py
@@ -29,28 +29,28 @@ class User:
   def __init__(self,name,email):
     self.name = name
     self.email = email
-    self.account = [BankAccount(0.05)]
+    self.accounts = [BankAccount(0.05)]
 
   def make_deposit(self,amount,num):
-    self.account[num].deposit(amount)
+    self.accounts[num].deposit(amount)
     return self
 
   def make_withdraw(self,amount,num):
-    self.account[num].withdraw(amount)
+    self.accounts[num].withdraw(amount)
     return self
 
   def display_user_balance(self,num):
-    print('User: ' + self.name + ', Balance :' + str(self.account[num].balance))
+    print('User: ' + self.name + ', Balance :' + str(self.accounts[num].balance))
 
   def transfer_money(self, receiver, amount, num):
-    if self.account[num].balance >= amount:
-      self.account[num].balance -= amount
-      receiver.account[0].balance += amount
+    if self.accounts[num].balance >= amount:
+      self.accounts[num].withdraw(amount)
+      receiver.accounts[0].deposit(amount)
     else:
       print('Insuficcient balance')
   
   def create_account(self):
-    self.account.append(BankAccount(0.05))
+    self.accounts.append(BankAccount(0.05))
 
 guido = User('Guido van Rossum', 'guido@python.com')
 monty = User('Monty Python', 'monty@python.com')
